41e9bc8fd81d567b8e0f698fd2ef45e592f04f21
majdsafira <majdsafira@gmail.com>
2022-07-28 18:39:35 +0300

    majd

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,319 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    
+ <div id="loading">
+  <div id="loading-center">
+      <img src="images/loader.gif" alt=""/>
+ </div>
+</div>
+
+
+<header id="header" class="defualt">
+<div class="topbar">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-12">
+				<div class="topbar-left text-lg-start text-center">
+				   <ul class="list-inline">
+             <li> <i class="fa fa-envelope-o"> </i> support@website.com</li>
+             <li> <i class="fa fa-clock-o"></i> Mon - Sat 8.00 - 18.00. Sunday CLOSED</li>
+           </ul>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-12">
+				<div class="topbar-right text-lg-end text-center">
+					 <ul class="list-inline">
+             <li> <i class="fa fa-phone"></i> (007) 123 456 7890</li>
+             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+             <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+             <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+           </ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="menu">
+   <nav id="menu" class="mega-menu">
+    <section class="menu-list-items">
+     <div class="container">
+      <div class="row">
+       <div class="col-md-12 position-relative">
+        <ul class="menu-logo">
+            <li>
+                <a href="index-2.html"><img id="logo_img" src="images/logo-light.png" alt="logo"/> </a>
+            </li>
+        </ul>
+        <ul class="menu-links">
+            <li class="active"><a href="javascript:void(0)"> Home <i class="fa fa-angle-down fa-indicator"></i></a>
+                <ul class="drop-down-multilevel">
+                    <li class="active"><a href="index-2.html">Home 1</a></li>
+                    <li><a href="index-3.html">Home 2</a></li>
+                    <li><a href="index-4.html">Home 3</a></li>
+                    <li><a href="index-5.html">Home 4</a></li>
+                    <li><a href="index-6.html">Home 5</a></li>
+                    <li><a href="index-7.html">Home 6</a></li>
+                    <li><a href="index-8.html">Home 7</a></li>
+                    <li><a href="index-9.html">Home 8</a></li>
+                    <li><a href="index-10.html">Home 9</a></li>
+                    <li><a href="index-11.html">Home 10</a></li>
+                    <li><a href="index-car-service.html">Car service </a></li>
+                    <li><a href="index-car-directory.html"> Car directory</a></li>
+                    <li><a href="index-car-listing.html">Car listing </a></li>
+                    <li><a href="index-landing-page.html">landing page</a></li>
+                </ul>
+            </li>
+            <li><a href="javascript:void(0)">Pages <i class="fa fa-angle-down fa-indicator"></i></a>
+                <div class="drop-down menu-bg grid-col-12">
+                    <div class="grid-row">
+                        <div class="grid-col-3">
+                            <ul>
+                              <li><a href="about-01.html">About 01</a></li>
+                              <li><a href="about-02.html">About 02</a></li>
+                              <li><a href="service-01.html">service 01</a></li>
+                              <li><a href="service-02.html">service 02</a></li>
+                              <li><a href="career.html">career</a></li>
+                            </ul>
+                        </div>
+                        <div class="grid-col-3">
+                            <ul>
+                              <li><a href="team-01.html">team 01</a></li>
+                              <li><a href="team-02.html">team 02</a></li>
+                              <li><a href="contact-01.html">contact 01</a></li>
+                              <li><a href="contact-02.html">contact 02</a></li>
+                              <li><a href="faq.html">faq</a></li>
+                            </ul>
+                        </div>
+                        <div class="grid-col-3">
+                            <ul>
+                              <li><a href="login.html">login</a></li>
+                              <li><a href="register.html">register</a></li>
+                              <li><a href="error-404.html">error 404</a></li>
+                              <li><a href="coming-soon.html">coming soon</a></li>
+                              <li><a href="typography.html">typography</a></li>
+                            </ul>
+                        </div>
+                        <div class="grid-col-3">
+                            <ul>
+                              <li><a href="page-left-sidebar.html">page left sidebar </a></li>
+                              <li><a href="page-right-sidebar.html">page right sidebar </a></li>
+                              <li><a href="page-both-sidebar.html">page both sidebar </a></li>
+                              <li><a href="terms-and-conditions.html">terms and conditions </a></li>
+                              <li><a href="privacy-policy.html">privacy policy </a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </li>
+            <li><a href="javascript:void(0)">blog <i class="fa fa-angle-down fa-indicator"></i></a>
+                <ul class="drop-down-multilevel">
+                    <li><a href="javascript:void(0)">blog classic <i class="fa fa-angle-right fa-indicator"></i> </a>
+                        <ul class="drop-down-multilevel">
+                           <li><a href="blog-left-sidebar.html">left sidebar</a></li>
+                           <li><a href="blog-right-sidebar.html">right sidebar</a></li>
+                           <li><a href="blog-fullwidth.html">fullwidth</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="javascript:void(0)">Masonry  <i class="fa fa-angle-right fa-indicator"></i> </a>
+                        <ul class="drop-down-multilevel">
+                            <li><a href="blog-masonry-2-collums.html"> 2 column </a> </li>
+                            <li><a href="blog-masonry-3-collums.html"> 3 column </a> </li>
+                            <li><a href="blog-masonry-4-collums.html"> 4 column </a> </li>
+                            <li><a href="blog-masonry-left-sidebar.html">left sidebar</a></li>
+                           <li><a href="blog-masonry-right-sidebar.html">right sidebar</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="javascript:void(0)">blog single <i class="fa fa-angle-right fa-indicator"></i> </a>
+                        <ul class="drop-down-multilevel">
+                            <li><a href="blog-single-01.html">blog single 01 </a> </li>
+                            <li><a href="blog-single-02.html">blog single 02 </a> </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="javascript:void(0)">Car listing  <i class="fa fa-angle-down fa-indicator"></i></a>
+                <ul class="drop-down-multilevel">
+                    <li><a href="listing-01.html">listing 01</a></li>
+                    <li><a href="listing-02.html">listing 02</a></li>
+                </ul>
+            </li>
+            <li><a href="javascript:void(0)">Car details  <i class="fa fa-angle-down fa-indicator"></i></a>
+                <ul class="drop-down-multilevel">
+                    <li><a href="details-01.html">details 01</a></li>
+                    <li><a href="details-02.html">details 02</a></li>
+                </ul>
+            </li>
+            <li><a href="javascript:void(0)"> Contact <i class="fa fa-angle-down fa-indicator"></i></a>
+                <ul class="drop-down-multilevel right-menu">
+                   <li><a href="contact-01.html">contact 01</a></li>
+                   <li><a href="contact-02.html">contact 02</a></li>
+                </ul>
+            </li>
+            <li>
+              <div class="search-top"> <a class="search-btn not_click d-none d-lg-block" href="javascript:void(0);">Search Button</a>
+                <div class="search-box not-click">
+                  <div class="row">
+                    <div class="col-xl-2 col-md-4 col-sm-6">
+                     <div class="selected-box">
+                      <select class="selectpicker">
+                      <option>Make </option>
+                      <option>BMW</option>
+                      <option>Honda </option>
+                      <option>Hyundai </option>
+                      <option>Nissan </option>
+                      <option>Mercedes Benz </option>
+                     </select>
+                    </div>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-6">
+                      <div class="selected-box">
+                         <select class="selectpicker">
+                          <option>Model</option>
+                          <option>3-Series</option>
+                          <option>Carrera</option>
+                          <option>GT-R</option>
+                          <option>Cayenne</option>
+                          <option>Mazda6</option>
+                          <option>Macan</option>
+                         </select>
+                       </div>
+                      </div>
+                      <div class="col-xl-2 col-md-4 col-sm-6">
+                       <div class="selected-box">
+                       <select class="selectpicker">
+                        <option>Year</option>
+                        <option>2010</option>
+                        <option>2011</option>
+                        <option>2012</option>
+                        <option>2013</option>
+                        <option>2014</option>
+                        <option>2015</option>
+                        <option>2016</option>
+                       </select>
+                      </div>
+                        </div>
+                       <div class="col-xl-2 col-md-4 col-sm-6">
+                        <div class="selected-box">
+                         <select class="selectpicker">
+                            <option>Body style</option>
+                            <option>2dr Car</option>
+                            <option>4dr Car</option>
+                            <option>Convertible</option>
+                            <option>Sedan</option>
+                            <option>Sports Utility</option>
+                           </select>
+                          </div>
+                        </div>
+                       <div class="col-xl-2 col-md-4 col-sm-6">
+                        <div class="selected-box">
+                         <select class="selectpicker">
+                            <option>Vehicle Status</option>
+                            <option>Condition</option>
+                            <option>All Conditions</option>
+                            <option>Condition</option>
+                            <option>Brand New</option>
+                            <option>Slightly Used</option>
+                            <option>Used</option>
+                           </select>
+                         </div>
+                        </div>
+                       <div class="col-xl-2 col-md-4 col-sm-6">
+                        <div class="text-center">
+                          <button class="button red" type="button">Search</button>
+                      </div>
+                    </div>
+                  </div>
+                 </div>
+               </div>
+              </li>
+            </ul>
+           </div>
+          </div>
+         </div>
+        </section>
+       </nav>
+     </div>
+    </header>
+
+    <section class="welcome-block objects-car page-section-ptb white-bg">
+ <div class="objects-left left"><img class="img-fluid objects-1" src="images/objects/01.jpg" alt=""/></div>
+ <div class="objects-right right"><img class="img-fluid objects-2" src="images/objects/02.jpg" alt=""/></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+         <div class="section-title">
+           <span>Welcome to our website</span>
+           <h2>Dealeractive</h2>
+           <div class="separator"></div>
+           <p>Car Dealer is the best premium HTML5 Template. We provide everything you need to build an <strong>Amazing dealership website</strong>  developed especially for car sellers, dealers or auto motor retailers. You can use this template for creating website based on any framework and any language.</p> </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-3 col-md-6">
+        <div class="feature-box text-center">
+          <div class="icon">
+            <i class="glyph-icon flaticon-beetle"></i>
+          </div>
+          <div class="content">
+            <h6>All brands</h6>
+            <p>Galley simply dummy text lorem Ipsum is of the printin  k a of type and</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="feature-box text-center">
+          <div class="icon">
+            <i class="glyph-icon flaticon-interface-1"></i>
+          </div>
+          <div class="content">
+            <h6>Free Support</h6>
+            <p>Text of the printin lorem ipsum the is simply k a type text and galley of</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="feature-box text-center">
+          <div class="icon">
+            <i class="glyph-icon flaticon-key"></i>
+          </div>
+          <div class="content">
+            <h6>Dealership</h6>
+            <p>Printin k a of type and lorem Ipsum is simply dummy text of the galley </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="feature-box text-center">
+          <div class="icon">
+            <i class="glyph-icon flaticon-wallet"></i>
+          </div>
+          <div class="content">
+            <h6>affordable</h6>
+            <p>The printin k a galley Lorem Ipsum is type and simply dummy text of</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+       <div class="col-md-12">
+          <div class="halp-call text-center">
+            <img class="img-fluid" src="images/team/01.jpg" alt=""/>
+            <span>Have any question ?</span>
+            <h2 class="text-red">(007) 123 456 7890</h2>
+          </div>
+       </div>
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default Home
