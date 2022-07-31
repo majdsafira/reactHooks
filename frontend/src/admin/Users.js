1c6810a8b63c382edfefdc66df9e20a6ef8082b1
salambelal <salambelal99@gmail.com>
2022-08-01 00:08:20 +0100

    main

--- frontend/src/admin/Users.js
+++ frontend/src/admin/Users.js
@@ -27,105 +27,42 @@ const Users = () => {
     <table class="table table-striped">
                       <thead>
                         <tr>
-                          <th> User </th>
-                          <th> First name </th>
-                          <th> Progress </th>
-                          <th> Amount </th>
+                          <th> Name </th>
+                          <th> Email </th>
+                          <th> Image </th>
+                          <th> Phone </th>
                           <th> Deadline </th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td class="py-1">
-                            <img src="assets/images/faces-clipart/pic-1.png" alt="image" />
-                          </td>
-                          <td> Herman Beck </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-success" role="progressbar" style={{width: 25, ariavaluenow:25, ariavaluemin:0, ariavaluemax:100}}></div>
-                            </div>
-                          </td>
-                          <td> $ 77.99 </td>
-                          <td> May 15, 2015 </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="assets/images/faces-clipart/pic-2.png" alt="image" />
-                          </td>
-                          <td> Messsy Adam </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-danger" role="progressbar" style={{width: 75, ariavaluenow:75, ariavaluemin:0 ,ariavaluemax:100}}></div>
-                            </div>
-                          </td>
-                          <td> $245.30 </td>
-                          <td> July 1, 2015 </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="assets/images/faces-clipart/pic-3.png" alt="image" />
-                          </td>
-                          <td> John Richards </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-warning" role="progressbar" style={{width: 90, ariavaluenow:90, ariavaluemin:0, ariavaluemax:100}}></div>
-                            </div>
-                          </td>
-                          <td> $138.00 </td>
-                          <td> Apr 12, 2015 </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="../../assets/images/faces-clipart/pic-4.png" alt="image" />
-                          </td>
-                          <td> Peter Meggik </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-primary" role="progressbar" style={{width: 50, ariavaluenow:50,ariavaluemin:0, ariavaluemax:100}}></div>
-                            </div>
-                          </td>
-                          <td> $ 77.99 </td>
-                          <td> May 15, 2015 </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="assets/images/faces-clipart/pic-1.png" alt="image" />
-                          </td>
-                          <td> Edward </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-danger" role="progressbar" style={{width: 3, ariavaluenow:35, ariavaluemin:0, ariavaluemax:100}}></div>
-                            </div>
-                          </td>
-                          <td> $ 160.25 </td>
-                          <td> May 03, 2015 </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="assets/images/faces-clipart/pic-2.png" alt="image" />
-                          </td>
-                          <td> John Doe </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-info" role="progressbar" style={{width: 65, ariavaluenow:65, ariavaluemin:0 ,ariavaluemax:100}}></div>
-                            </div>
-                          </td>
-                          <td> $ 123.21 </td>
-                          <td> April 05, 2015 </td>
-                        </tr>
-                        <tr>
-                          <td class="py-1">
-                            <img src="assets/images/faces-clipart/pic-3.png" alt="image" />
-                          </td>
-                          <td> Henry Tom </td>
-                          <td>
-                            <div class="progress">
-                              <div class="progress-bar bg-warning" role="progressbar" style={{width: 20, ariavaluenow:20, ariavaluemin:0, ariavaluemax:100}}></div>
-                            </div>
-                          </td>
-                          <td> $ 150.00 </td>
-                          <td> June 16, 2015 </td>
-                        </tr>
+                      {data.map((user) => {
+                    return (
+                      <tr key={user.id}>
+                        <th scope="row">{user.id}</th>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+
+                        <td>
+                          
+                          <img
+                            src={
+                              "http://127.0.0.1:8000/category_image/" +
+                              user.image
+                            }
+                            alt=""
+                            style={{ width: "100px" }}
+                          />
+                        </td>
+                        
+                        <td>{user.phone}</td>
+                      
+                        {/* <td>{user.user_status}</td> */}
+                        {/* <Link  to={`/singlecategory/${product.id}`} className="btn btn-primary mt-auto align-self-start" >Edite</Link>*/}
+                        <Link className="btn btn-primary mt-auto align-self-start" onClick={() =>deleteUser(user.id)} to=''> Delete</Link> 
+                        {/* <td> <Link to={'http://127.0.0.1:8000/api/deletecategory/'+category.id} className="btn btn-primary mt-auto align-self-start">Delete</Link></td>          */}
+                                     </tr>
+                    );
+                  })}
                       </tbody>
                     </table>
     </>
